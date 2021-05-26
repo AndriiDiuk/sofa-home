5bb8cc6eb1304d6f0ee4f9567d284a0bef8c0ea8
Andrii Daneliuk <72430849+AndriiDuk@users.noreply.github.com>
2021-05-26 16:22:32 +0300

    Html Code

--- js/index.js
+++ js/index.js
@@ -2,46 +2,20 @@ const productsPostJson =
 	`[
     {
         "id": "1",
-        "title": "<b>15% off</b> on Tables & Cabinets",
-        "imgUrl": "img/tables-cabinets.png"
+        "title": "15% off on Tables & Cabinets",
+        "imgUrl": "tables-cabinets.png"
     },
     {
         "id": "2",
         "title": "All-White Collection in Store",
-        "imgUrl": "img/all-white-collection.png"
+        "imgUrl": "all-white-collection.png"
     },
     {
         "id": "3",
-        "title": "<b>NEW!</b> Modern Collection by ARM",
-        "imgUrl": "img/modern-collection.png"
+        "title": "NEW! Modern Collection by ARM",
+        "imgUrl": "modern-collection.png"
     }
 ]`;
-
-const productsJson =
-	`[
-    {
-        "id": "1",
-        "title": "Lord armchair, Miedel Home",
-        "imgUrl": "img/lord-armchair.png",
-        "description": "Lord armchair Miedel Home",
-        "price": 1200
-    },
-    {
-        "id": "2",
-        "title": "Banana",
-        "imgUrl": "img/ultimate-green-chair.png",
-        "description": "Ultimate Green chair, XODO",
-        "price": 900
-    },
-    {
-        "id": "3",
-        "title": "Girl",
-        "imgUrl": "img/valetta-armchair.png",
-        "description": "Valetta armchair, ZIX studio",
-        "price": 3100
-    }
-]`;
-
 // Product-post
 function postCardProducts(productsPost) {
 	const productsPostCard = document.querySelector(".product-post");
@@ -49,7 +23,7 @@ function postCardProducts(productsPost) {
 	for (const product of productsPost) {
 		productsPostCard.innerHTML += `
 		<article class="product-post-card">
-				<img src="${product.imgUrl}" alt="${product.title}">
+				<img src="img/${product.imgUrl}" alt="${product.title}">
 				<h2>${product.title}</h2>
 				<a href="#">Learn more</a>
 			</article>
@@ -58,15 +32,31 @@ function postCardProducts(productsPost) {
 }
 const productsPost = JSON.parse(productsPostJson);
 postCardProducts(productsPost);
-
+// ------------------------------------------
 // Best-selling Products
-function renderProducts(products) {
+let products;
+
+// function fetchProducts() {
+// 	fetch('products.json')
+// 		.then(response => response.json())
+// 		.then(productsFromServer => products = productsFromServer)
+// 		.then(() => renderProducts())
+// 	   .catch(err => alert(err.message));
+// }
+async function fetchProducts() {
+	const response = await fetch('products.json');
+	products = await response.json();
+	renderProducts();
+}
+fetchProducts();
+
+function renderProducts() {
 	const productsBestSelling = document.querySelector(".box-selling-card");
 	productsBestSelling.innerHTML = '';
 	for (const product of products) {
 		productsBestSelling.innerHTML += `
 			<article class="best-selling-card">
-				<img src="${product.imgUrl}" alt="${product.title}">
+				<img src="img/${product.imgUrl}" alt="${product.title}">
 				<h2>${product.title}</h2>
 				<p>${product.price} USD</p>
 				<a href="#" class="selling-btn">order now</a>
@@ -74,6 +64,3 @@ function renderProducts(products) {
 			`;
 	}
 }
-
-const products = JSON.parse(productsJson);
-renderProducts(products);
